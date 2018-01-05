98e361615b85f2bc7a648a5e1b18daff991c15b7
Michael C. Szczepanik <mcs@gemko.com>
2018-01-04 19:53:30 -0500

    Updates to display stats and modifiers in manager view

--- classes/DDPlayer.php
+++ classes/DDPlayer.php
@@ -33,11 +33,17 @@ class DDPlayer extends DDObject {
 	protected $currentHP;
 	protected $armorClass;
 	protected $strength;
+	protected $strengthModifier;
 	protected $dexterity;
+	protected $dexterityModifier;
 	protected $constitution;
+	protected $constitutionModifier;
 	protected $intelligence;
+	protected $intelligenceModifier;
 	protected $wisdom;
+	protected $wisdomModifier;
 	protected $charisma;
+	protected $charismaModifier;
 	protected $initiative;
 	protected $imageLocation;
 	protected $statusFlag;
@@ -69,6 +75,14 @@ class DDPlayer extends DDObject {
 		$this->statusFlag = $playerRecord['statusFlag'];
 		$this->lastChange = $playerRecord['lastChange'];
 		$this->creationDate = $playerRecord['creationDate'];
+		
+		// calculate modifiers
+		$this->strengthModifier = $this->calculateModifier($this->strength);
+		$this->dexterityModifier = $this->calculateModifier($this->dexterity);
+		$this->constitutionModifier = $this->calculateModifier($this->constitution);
+		$this->intelligenceModifier = $this->calculateModifier($this->intelligence);
+		$this->wisdomModifier = $this->calculateModifier($this->wisdom);
+		$this->charismaModifier = $this->calculateModifier($this->charisma);
 		
 		// check for hp override
 		$battleCount = $this->database->getUniqueCount("dragons.battleHeader", "entryId", array("statusFlag"=>"A"));
@@ -144,10 +158,54 @@ class DDPlayer extends DDObject {
 				<em>' . $this->playerRace . ' / ' . $this->playerClass . '</em><br />
 				AC: ' . $this->armorClass . '<br />
 				HP: ' . $this->currentHP . '/' . $this->maxHP . '<br />
-				Initiative: ' . number_format($this->initiative, 0, "", "") . '
-				<div class="blueButton" onClick="setInit(\'P\', ' . $playerReference . ');">Set Init</div> <div class="redButton" onClick="takeDamage(\'P\', ' . $playerReference . ');">Take Damage</div> <div class="greenButton" onClick="heal(\'P\', ' . $playerReference . ');">Heal</div>
+				Initiative: ' . number_format($this->initiative, 0, "", "") . '<br />
+				Str: ' . $this->strength . ' (' . $this->strengthModifier . ') | Dex: ' . $this->dexterity . ' (' . $this->dexterityModifier . ')<br />
+				Con: ' . $this->constitution . 	' (' . $this->constitutionModifier . ') | Int: ' . $this->intelligence . ' (' . $this->intelligenceModifier . ')<br />
+				Wis: ' . $this->wisdom . ' (' . $this->wisdomModifier . ') | Cha: ' . $this->charisma . ' (' . $this->charismaModifier . ')<br />
+				<div class="blueButton" onClick="setInit(\'P\', ' . $playerReference . ');">Set Init</div><br /><div class="redButton" onClick="takeDamage(\'P\', ' . $playerReference . ');">Take Damage</div><br /><div class="greenButton" onClick="heal(\'P\', ' . $playerReference . ');">Heal</div>
 			</div>
 		';
+	}
+	//------------------------------------------------------------------------
+	
+	
+	//------------------------------------------------------------------------
+	// calculate modifier
+	//------------------------------------------------------------------------
+	private function calculateModifier($value) {
+		if ($value == 1) {
+			return -5;
+		} else if ($value == 2 || $value == 3) {
+			return -4;
+		} else if ($value == 4 || $value == 5) {
+			return -3;
+		} else if ($value == 6 || $value == 7) {
+			return -2;
+		} else if ($value == 8 || $value == 9) {
+			return -1; 
+		} else if ($value == 10 || $value == 11) {
+			return 0;
+		} else if ($value == 12 || $value == 13) {
+			return 1;
+		} else if ($value == 14 || $value == 15) {
+			return 2;
+		} else if ($value == 16 || $value == 17) {
+			return 3;
+		} else if ($value == 18 || $value == 19) {
+			return 4;
+		} else if ($value == 20 || $value == 21) {
+			return 5;
+		} else if ($value == 22 || $value == 23) {
+			return 6;
+		} else if ($value == 24 || $value == 25) {
+			return 7;
+		} else if ($value == 26 || $value == 27) {
+			return 8;
+		} else if ($value == 28 || $value == 29) {
+			return 9;
+		} else if ($value >= 30) {
+			return 10;
+		}
 	}
 	//------------------------------------------------------------------------
 }
