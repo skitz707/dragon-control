856e6b3db191043b777e7f5ae41fb6a8bf0a436b
Michael C. Szczepanik <mcs@gemko.com>
2018-02-28 10:56:52 -0500

    Layout/css changes

--- includes/header.php
+++ includes/header.php
@@ -7,5 +7,18 @@
 		<script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
 	</head>
 	<body>
+		<!-- control bar at top of the screen -->
+		<div id="controlBar">
+			<div id="controlLeft">
+				<span id="menuBox" onClick="launchMenu();"></span> <?php print($crumbTrail); ?>
+			</div>
+			<div id="controlRight">
+				<?php if (isset($_SESSION['username'])) { print(date("l, F jS h:iA")); ?> | User: <?php print(strtoupper($_SESSION['username'])); ?> | <a href="http://192.168.2.10:10080/logout.php">Logout</a><?php } ?>
+			</div>
+		</div>
+
+		<!-- popup menu -->
+		<div id="menu"><?php if (isset($_SESSION['username'])) { print($user->getMenuOptions($_SESSION['username'])); } ?></div>
+		
 		<div id="mainContainer">
 		
--- index.php
+++ index.php
@@ -32,27 +32,30 @@ require_once("classes/DDDatabase.php");
 //-------------------------------------------------------------------------------------------
 $database = new DDDatabase();
 $pageTitle = "Dragon Control - Login";
+$crumbTrail = "";
 
 require_once("includes/header.php");
 ?>
 
 <div id="mainContent">
-	<span class="largeHeading">Dragon-Control Login</span>
-	<br /><br />
-	<form method="post" action="login.php" id="loginForm">
-	<table style="margin-left: auto; margin-right: auto;">
-		<tr>
-			<td>Email</td>
-			<td><input type="text" id="emailAddress" name="emailAddress" size="20" /></td>
-		</tr>
-		<tr>
-			<td>Password</td>
-			<td><input type="password" id="password" name="password" size="20" /></td>
-		</tr>
-	</table>
-	<br />
-	<div class="blueButton" onClick="document.getElementById('loginForm').submit();">Login</div>
-	</form>
+	<div id="loginFormDiv" style="margin-top: 90px;">
+		<span style="font-size: 36pt; color: #ffffff;">Dragon Control Login</span>
+		<br /><br /><br />
+		<form method="post" action="login.php" id="loginForm">
+		<table style="margin-left: auto; margin-right: auto;">
+			<tr>
+				<td style="font-size: 24pt;">Email</td>
+				<td><input type="text" id="emailAddress" name="emailAddress" size="30" class="loginTextField" style="margin-left: 15px;" /></td>
+			</tr>
+			<tr>
+				<td style="font-size: 24pt;">Password</td>
+				<td><input type="password" id="password" name="password" size="30" class="loginTextField" style="margin-left: 15px;" /></td>
+			</tr>
+		</table>
+		<br />
+		<div class="redButton" onClick="document.getElementById('loginForm').submit();"><span style="font-size: 18pt;">Login</div>
+		</form>			
+	</div>
 </div>
 
 <?php
